cc3b7582f62e7f88a1e871920d8c1ad36b6a0312
KasperTell <kaspertell@gmail.com>
2024-04-30 10:03:55 +0200

    UserModel

---
+++ Klient/infrastructure/DataModels/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace infrastructure.DataModels;
+
+public class UserModel
+{
+    public string username { get; set; }
+    public string password { get; set; }
+    public string address { get; set; }
+    public int zipcode { get; set; }
+    public int? cvr { get; set; }
+}
