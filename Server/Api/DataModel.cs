e21aba483fc38df9d8450be2e4a1622f312fe4d5
KasperTell <kaspertell@gmail.com>
2024-04-30 09:47:23 +0200

    MQTT sender data til database

---
+++ Server/Api/DataModel.cs
@@ -0,0 +1,11 @@
+﻿namespace api;
+
+public class DataModel
+{
+    public int data_id { get; set; }
+    public string client_id { get; set; }
+    public double data { get; set; }
+    public DateTime date { get; set; }
+    public string alarm { get; set; }
+}
+
---
+++ Server/Api/Utilities.cs
@@ -0,0 +1,21 @@
+﻿namespace api;
+
+public class Utilities
+{
+    /*
+     * Private static readonly Uri instance initialized with the PostgreSQL connection string.
+     */
+    private static readonly Uri Uri = new(Environment.GetEnvironmentVariable("pgconn")!);
+
+    /*
+     * Publicly accessible properly formatted PostgreSQL connection string.
+     */
+    public static readonly string
+        ProperlyFormattedConnectionString = string.Format(
+            "Server={0};Database={1};User Id={2};Password={3};Port={4};Pooling=false;",
+            Uri.Host,
+            Uri.AbsolutePath.Trim('/'),
+            Uri.UserInfo.Split(':')[0],
+            Uri.UserInfo.Split(':')[1],
+            Uri.Port > 0 ? Uri.Port : 5432);
+}
