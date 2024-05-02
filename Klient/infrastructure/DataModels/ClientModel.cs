2977c5887a31836849be8f4d9991d90bc21f7d23
KasperTell <kaspertell@gmail.com>
2024-05-02 09:21:07 +0200

    TIlføjelse af ClientRepository og Repositories mappe, samt datamodel til client

---
+++ Klient/infrastructure/DataModels/ClientModel.cs
@@ -0,0 +1,9 @@
+﻿namespace infrastructure.DataModels;
+
+public class ClientModel
+{
+    public string client_id { get; set; }  
+    public string client_name { get; set; }
+    public decimal max_value { get; set; }            
+    public decimal min_value { get; set; }   
+}
---
+++ Klient/infrastructure/Repositories/ClientRepository.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using infrastructure.DataModels;
+using Npgsql;
+
+namespace infrastructure.Repositories;
+
+public class ClientRepository
+{
+    public readonly NpgsqlDataSource _DataSource;
+
+    public ClientRepository(NpgsqlDataSource dataSource)
+    {
+        _DataSource = dataSource;
+    }
+
+    public ClientModel CreateClient(ClientModel clientModel)
+    {
+        var sql = "INSERT INTO ph.client(client_id, client_name, max_value, min_value) VALUES (@client_id, @client_name, @max_value, @min_value);";
+
+        using (var conn = _DataSource.OpenConnection())
+        {
+           return conn.QueryFirst(sql,
+                new { client_id = clientModel.client_id, client_name = clientModel.client_name, max_value = clientModel.max_value, min_value = clientModel.min_value });
+        }
+    } 
+
+
+}
+
