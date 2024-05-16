763bd1d896c4a4c08e847c67589822fd53e95220
KasperTell <kaspertell@gmail.com>
2024-05-16 10:03:42 +0200

    Umuligegøre tilgåelse af sider hvis man ikke er logged in

---
+++ Klient/Frontend/src/app/AuthGuard.ts
@@ -0,0 +1,20 @@
+﻿import {Injectable} from "@angular/core";
+import {Router} from "@angular/router";
+import {DataService} from "./Services/Data.service";
+
+@Injectable()
+export class AuthGuard {
+
+  constructor(private router: Router, readonly dataService: DataService) {}
+
+  canActivate() {
+    if (this.dataService.loginUser != "") {
+      // logged in so return true'
+      return true;
+    }
+
+    // not logged in so redirect to login page with the return url
+    this.router.navigate(['home']);
+    return false;
+  }
+}
